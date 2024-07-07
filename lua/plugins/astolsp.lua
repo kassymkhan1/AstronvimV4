5af88b5b6795cdacc882690323d4de9263113a42
kassymkhan1 <kassymkhan24@gmail.com>
2024-07-07 23:36:39 +0300

    update

--- lua/plugins/astolsp.lua
+++ lua/plugins/astolsp.lua
@@ -4,36 +4,18 @@ return {
   ---@type AstroLSPOpts
   opts = {
     -- Configuration table of features provided by AstroLSP
-    features = {
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
-    },
+    -- features = {
+    --   codelens = true, -- enable/disable codelens refresh on start
+    --   inlay_hints = false, -- enable/disable inlay hints on start
+    --   semantic_tokens = true, -- enable/disable semantic token highlighting
+    -- },
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
         enabled = false, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
       },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
-      },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
+      timeout_ms = 100, -- default format timeout
     }
-
-    -- enable servers that you already have installed without mason
-    
     }
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
   }
